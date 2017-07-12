d52fe8d6fc86e51635cba02f6890fa7ec2f286b4
msusur <mail@mertsusur.com>
2017-07-12 22:47:04 +0100

    Adds landing section component

---
+++ src/src/app/components/landing-section/landing-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-landing-section',
+  templateUrl: './landing-section.component.html',
+  styleUrls: ['./landing-section.component.scss']
+})
+export class LandingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
