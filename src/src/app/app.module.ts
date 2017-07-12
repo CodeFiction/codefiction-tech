a40637cb7cfc9e2ca1afecfea044a817c194ff57
msusur <mail@mertsusur.com>
2017-07-12 22:17:17 +0100

    Add static assets and update index.html

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { LandingSectionComponent } from './Components/landing-section/landing-section.component';
+import { AboutSectionComponent } from './Components/about-section/about-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    LandingSectionComponent,
+    AboutSectionComponent
   ],
   imports: [
     BrowserModule
