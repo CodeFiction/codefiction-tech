8afba0a00433dbd3afc09db223be8407e01b7b33
Mert Susur <mail@mertsusur.com>
2017-07-14 15:24:27 +0100

    Adds tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+let component: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,9 +18,72 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
+  describe('compiled template', () => {
+    it('should display menu bar', () => {
+      // Act
+      fixture.detectChanges();
+
+      // Assert
+      const menu: DebugElement = fixture.debugElement.query(
+        By.css('cf-menu-bar')
+      );
+      expect(menu).toBeTruthy();
+    });
+
+    it('should display landing section', () => {
+      // Act
+      fixture.detectChanges();
+
+      // Assert
+      const landing: DebugElement = fixture.debugElement.query(
+        By.css('cf-landing-section')
+      );
+      expect(landing).toBeTruthy();
+    });
+
+    it('should display podcasts section', () => {
+      // Act
+      fixture.detectChanges();
+
+      // Assert
+      const podcasts: DebugElement = fixture.debugElement.query(
+        By.css('cf-podcasts-section')
+      );
+      expect(podcasts).toBeTruthy();
+    });
+
+    it('should display about section', () => {
+      // Act
+      fixture.detectChanges();
+
+      // Assert
+      const about: DebugElement = fixture.debugElement.query(
+        By.css('cf-about-section')
+      );
+      expect(about).toBeTruthy();
+    });
+
+    it('should display page footer', () => {
+      // Act
+      fixture.detectChanges();
+
+      // Assert
+      const footer: DebugElement = fixture.debugElement.query(
+        By.css('cf-page-footer')
+      );
+      expect(footer).toBeTruthy();
+    });
+
+  });
 });
